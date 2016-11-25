a77cf7b2fc02545b7b7b3642db7404c69f2aa32d
sunarc <sunarc@mac-mini.local>
2016-11-25 12:15:35 +0530

    Commit

--- ExpandableCell/TableViewCell.swift
+++ ExpandableCell/TableViewCell.swift
@@ -13,7 +13,6 @@ class TableViewCell: UITableViewCell {
     @IBOutlet var lblDesc: UILabel?
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
---
+++ README.md
@@ -0,0 +1,2 @@
+# ExpandableCell
+Expandable cell according to lable or text size using Swift 3
